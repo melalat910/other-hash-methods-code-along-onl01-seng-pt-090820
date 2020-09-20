342bb2591c8ab40439c434a6fa80a98796841fab
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-20 01:39:48 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -10,7 +10,5 @@ require 'pry'
 
 def get_the_values(groceries)
   #code your solution here!
-  groceries = {fruit: "Banana", vegetable: "Broccoli", dessert: "Cookie"}
-
-  groceries.values
+  groceries.vaalue 
 end
